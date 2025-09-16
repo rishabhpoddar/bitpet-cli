15f02fc8300b7ef67e558efe484923319065992b
rishabhpoddar <rishabh.poddar@gmail.com>
2025-09-16 10:52:14 +0530

    refactors some code to add authenticated handlers

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,119 @@ struct Args {
     command: Commands,
 }
 
+/// Result type for command operations
+type CommandResult = Result<(), Box<dyn std::error::Error>>;
+
+/// Helper function to check if user is authenticated
+fn require_auth(config: &Config) -> Result<(), &'static str> {
+    if config.user.is_some() {
+        Ok(())
+    } else {
+        Err("Please login first using 'pet login'")
+    }
+}
+
+/// Execute a command that requires authentication
+fn execute_authenticated_command<F>(config: &Config, operation: F) -> CommandResult
+where
+    F: FnOnce() -> CommandResult,
+{
+    match require_auth(config) {
+        Ok(()) => operation(),
+        Err(msg) => {
+            println!("{}", msg);
+            Ok(())
+        }
+    }
+}
+
+// Command handlers
+fn handle_login(config: &Config) -> CommandResult {
+    if let Some(user) = &config.user {
+        println!("You are already logged in with email: {}", user.email);
+    } else {
+        // TODO: Implement login logic
+        println!("Login functionality not yet implemented");
+    }
+    Ok(())
+}
+
+fn handle_logout(config: &mut Config) -> CommandResult {
+    if config.user.is_some() {
+        println!("Logging out...");
+        config.user = None;
+        config
+            .save()
+            .map_err(|e| Box::new(e) as Box<dyn std::error::Error>)
+    } else {
+        println!("Not logged in");
+        Ok(())
+    }
+}
+
+fn handle_whoami(config: &Config) -> CommandResult {
+    if let Some(user) = &config.user {
+        println!("Email: {}", user.email);
+    } else {
+        println!("Not logged in");
+    }
+    Ok(())
+}
+
+fn handle_new_pet() -> CommandResult {
+    // TODO: Implement new pet logic
+    println!("New pet functionality not yet implemented");
+    Ok(())
+}
+
+fn handle_remove_pet() -> CommandResult {
+    // TODO: Implement remove pet logic
+    println!("Remove pet functionality not yet implemented");
+    Ok(())
+}
+
+fn handle_status() -> CommandResult {
+    // TODO: Implement status logic
+    println!("Status functionality not yet implemented");
+    Ok(())
+}
+
+fn handle_feed() -> CommandResult {
+    // TODO: Implement feed logic
+    println!("Feed functionality not yet implemented");
+    Ok(())
+}
+
+fn handle_play() -> CommandResult {
+    // TODO: Implement play logic
+    println!("Play functionality not yet implemented");
+    Ok(())
+}
+
+fn handle_add_repo(path: &str) -> CommandResult {
+    // TODO: Implement add repo logic
+    println!(
+        "Add repo functionality not yet implemented for path: {}",
+        path
+    );
+    Ok(())
+}
+
+fn handle_remove_repo(path: &str) -> CommandResult {
+    // TODO: Implement remove repo logic
+    println!(
+        "Remove repo functionality not yet implemented for path: {}",
+        path
+    );
+    Ok(())
+}
+
+fn handle_list_repos() -> CommandResult {
+    // TODO: Implement list repos logic
+    println!("List repos functionality not yet implemented");
+    Ok(())
+}
+
 fn main() {
     let args = Args::parse();
 
@@ -26,105 +139,21 @@ fn main() {
     };
 
     let result = match args.command {
-        Commands::Login {} => {
-            if let Some(user) = &config.user {
-                println!("You are already logged in with email: {}", user.email);
-                Ok(())
-            } else {
-                // TODO:...
-                Ok(())
-            }
+        Commands::Login {} => handle_login(&config),
+        Commands::Logout {} => handle_logout(&mut config),
+        Commands::Whoami {} => handle_whoami(&config),
+        Commands::NewPet {} => execute_authenticated_command(&config, || handle_new_pet()),
+        Commands::RemovePet {} => execute_authenticated_command(&config, || handle_remove_pet()),
+        Commands::Status {} => execute_authenticated_command(&config, || handle_status()),
+        Commands::Feed {} => execute_authenticated_command(&config, || handle_feed()),
+        Commands::Play {} => execute_authenticated_command(&config, || handle_play()),
+        Commands::AddRepo { path } => {
+            execute_authenticated_command(&config, || handle_add_repo(&path))
         }
-        Commands::Logout {} => {
-            if let Some(_user) = &config.user {
-                println!("Logging out...");
-                config.user = None;
-                config.save()
-            } else {
-                println!("Not logged in");
-                Ok(())
-            }
+        Commands::RemoveRepo { path } => {
+            execute_authenticated_command(&config, || handle_remove_repo(&path))
         }
-        Commands::Whoami {} => {
-            if let Some(user) = &config.user {
-                println!("Email: {}", user.email);
-            } else {
-                println!("Not logged in");
-            }
-            Ok(())
-        }
-        Commands::NewPet {} => {
-            if let Some(_user) = &config.user {
-                // TODO:...
-                Ok(())
-            } else {
-                println!("Please login first using 'pet login'");
-                Ok(())
-            }
-        }
-        Commands::RemovePet {} => {
-            if let Some(_user) = &config.user {
-                // TODO:...
-                Ok(())
-            } else {
-                println!("Please login first using 'pet login'");
-                Ok(())
-            }
-        }
-        Commands::Status {} => {
-            if let Some(_user) = &config.user {
-                // TODO:...
-                Ok(())
-            } else {
-                println!("Please login first using 'pet login'");
-                Ok(())
-            }
-        }
-        Commands::Feed {} => {
-            if let Some(_user) = &config.user {
-                // TODO:...
-                Ok(())
-            } else {
-                println!("Please login first using 'pet login'");
-                Ok(())
-            }
-        }
-        Commands::Play {} => {
-            if let Some(_user) = &config.user {
-                // TODO:...
-                Ok(())
-            } else {
-                println!("Please login first using 'pet login'");
-                Ok(())
-            }
-        }
-        Commands::AddRepo { path: _path } => {
-            if let Some(_user) = &config.user {
-                // TODO:...
-                Ok(())
-            } else {
-                println!("Please login first using 'pet login'");
-                Ok(())
-            }
-        }
-        Commands::RemoveRepo { path: _path } => {
-            if let Some(_user) = &config.user {
-                // TODO:...
-                Ok(())
-            } else {
-                println!("Please login first using 'pet login'");
-                Ok(())
-            }
-        }
-        Commands::ListRepos {} => {
-            if let Some(_user) = &config.user {
-                // TODO:...
-                Ok(())
-            } else {
-                println!("Please login first using 'pet login'");
-                Ok(())
-            }
-        }
+        Commands::ListRepos {} => execute_authenticated_command(&config, || handle_list_repos()),
     };
 
     // Handle any errors from config operations
